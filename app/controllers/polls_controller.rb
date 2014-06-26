30d46eb8fd916e957ebefc27d4ca68d3724b4cb4
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-06-26 15:54:38 -0400

    fixed polls params

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -70,6 +70,6 @@ class PollsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poll_params
-      params[:poll].permit(:nature, :attempted, :resolved_at)
+      params[:poll].permit(:owner, :question, :choices)
     end
 end
