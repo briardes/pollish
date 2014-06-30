ed4b655e9620732442f9690ca519fc40c1c99971
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-06-30 11:33:40 -0400

    Heroku setup

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,6 +1,8 @@
 class Poll < ActiveRecord::Base
 
   has_many :options
+
+  # took this code from mae and sebatians code
   accepts_nested_attributes_for :options, allow_destroy: true,
                                 reject_if: lambda { |option| option['answer'].blank? }
   validates :owner, presence: true
