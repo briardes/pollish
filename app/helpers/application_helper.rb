9d17bb4abc6cbf6d3518b66df7f3a2e15489e8a7
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-07-01 14:26:09 -0400

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # I got this code from Railscast episode 196
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
